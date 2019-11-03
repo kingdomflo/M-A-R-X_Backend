d502d1eeffcc1935d91adfa868add5c2dce5a2cb
Florian Gérard <kingdomflo16@gmail.com>
2019-11-03 15:38:33 +0100

    Clean up

--- app/Http/Controllers/RelationshipController.php
+++ app/Http/Controllers/RelationshipController.php
@@ -24,12 +24,6 @@ class RelationshipController extends Controller
   {
     //To have security
     $this->middleware('auth', ['only' => [
-      'getAll',
-      'create',
-      'getOne',
-      'update',
-      'delete',
-
       'getAllRelationshipType',
       'getAllRelationship',
       'createRelationship',
@@ -163,104 +157,6 @@ class RelationshipController extends Controller
 
     if ($relationship == null) {
       return Utils::errorResponseNotFound('relationship');
-    }
-
-    $relationship->delete();
-    return response()->json($relationship);
-  }
-
-
-
-
-
-
-
-
-
-  // old method, keep for the moment
-
-  public function getAll(Request $request)
-  {
-    $list = MarxRelationship::whereHas('user_relationship_type', function ($query) use ($request) {
-      $query->where('user_id', '=', $request->input('token_user_id'));
-    })
-      ->with('user_relationship_type')->get();
-
-    //return Relationship::map_array($list);
-    return $list;
-  }
-
-  public function getOne(Request $request, $id)
-  {
-    $relationship = MarxRelationship::where('id', '=', $id)
-      ->with('user_relationship_type')
-      ->first();
-
-    if ($relationship == null) {
-      return Utils::errorResponseNotFound('relationship');
-    }
-    //return response()->json(Relationship::map_single($relationship));
-    return response()->json($relationship);
-  }
-
-  public function create(Request $request)
-  {
-    $validator = Validator::make($request->all(), [
-      'name' => 'required',
-      'relationship_type_id' => 'required',
-    ]);
-
-    if ($validator->fails()) {
-      return Utils::errorResponse($validator->errors()->all());
-    }
-
-    $relationship = new MarxRelationship;
-    $relationship->name = $request->input('name');
-    $relationship->user_relationship_type_id = $request->input('relationship_type_id');
-    $relationship->save();
-
-    return response()->json($relationship);
-  }
-
-  public function update(Request $request, $id)
-  {
-    $validator = Validator::make($request->all(), [
-      'name' => 'required',
-      'relationship_type_id' => 'required',
-    ]);
-
-    if ($validator->fails()) {
-      return Utils::errorResponse($validator->errors()->all());
-    }
-
-    $relationship = MarxRelationship::find($id);
-
-    if ($relationship == null) {
-      return Utils::errorResponseNotFound('relationship');
-    }
-
-    if ($relationship->user_relationship_type->user_id != $request->input('token_user_id')) {
-      return Utils::errorResponseNotBelongToYou('relationship');
-    }
-
-    $relationship->name = $request->input('name');
-    $relationship->user_relationship_type_id = $request->input('relationship_type_id');
-    $relationship->save();
-
-    return response()->json($relationship);
-  }
-
-  public function delete(Request $request, $id)
-  {
-    // TODO test delete cascade after
-    $relationship = MarxRelationship::find($id);
-
-    if ($relationship == null) {
-      return Utils::errorResponseNotFound('relationship');
-    }
-
-    if ($relationship->user_id != $request->input('token_user_id')) {
-      return Utils::errorResponseNotBelongToYou('relationship');
     }
 
     $relationship->delete();
